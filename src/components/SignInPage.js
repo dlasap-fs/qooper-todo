8e8f4df13713ce8e43fc12bb8068514fd8f956d6
dlasap-fs <dlasap@fullscale.io>
2024-02-22 17:21:35 +0800

    added styled components, updated logic for error messages for Sign In form

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -1,11 +1,24 @@
 import { Navigate } from "react-router-dom";
 import { getLocalStorageItem } from "../utils/localStorageHelper";
 import { useState } from "react";
+import styled from "styled-components";
+
+const SignInPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function SignIn({ onSignIn }) {
   const user = JSON.parse(getLocalStorageItem("user"));
 
   const [userDetails, setUserDetails] = useState({
+    userName: "",
+    firstName: "",
+    lastName: "",
+  });
+  const [errors, setErrors] = useState({
     userName: "",
     firstName: "",
     lastName: "",
@@ -24,15 +37,49 @@ export default function SignIn({ onSignIn }) {
     });
   };
 
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    let isValid = true;
+    const newErrors = { ...errors };
+
+    if (userDetails.userName.trim() === "") {
+      newErrors.userName = "User name is required";
+      isValid = false;
+    }
+
+    if (userDetails.firstName.trim() === "") {
+      newErrors.firstName = "First name is required";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+
+    if (isValid) {
+      onSignIn(userDetails);
+    }
+  };
+
   return (
-    <>
+    <SignInPageContainer>
       <h1>Login in your account</h1>
-      <form>
-        <input placeholder="Username" id="userName" value={userDetails.userName} onChange={handleChangeInput} />
-        <input placeholder="First Name" id="firstName" value={userDetails.firstName} onChange={handleChangeInput} />
-        <input placeholder="Last Name" id="lastName" value={userDetails.lastName} onChange={handleChangeInput} />
-        <button onClick={() => onSignIn(userDetails)}>LOGIN</button>
+      <form onSubmit={handleSignIn}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "300px",
+          }}
+        >
+          <input placeholder="Username" id="userName" value={userDetails.userName} onChange={handleChangeInput} />
+          <span style={{ color: "red" }}>{errors.userName}</span>
+
+          <input placeholder="First Name" id="firstName" value={userDetails.firstName} onChange={handleChangeInput} />
+          <span style={{ color: "red" }}>{errors.firstName}</span>
+
+          <input placeholder="Last Name" id="lastName" value={userDetails.lastName} onChange={handleChangeInput} />
+          <button button="submit">LOGIN</button>
+        </div>
       </form>
-    </>
+    </SignInPageContainer>
   );
 }
